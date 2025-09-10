ecd9bf9db5e86d27c3836f474714ab021233f51c
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-09-10 11:32:01 -0400

    moving sln

--- ASN1/CoffeeOrder/obj/Debug/net9.0/CoffeeOrder.AssemblyInfo.cs
+++ ASN1/CoffeeOrder/obj/Debug/net9.0/CoffeeOrder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b282e88a88c0bc41c99f5335e6a4c7ba0366a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e328a0c4f11ccd4ca16c774c3730cd93f6fcbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeOrder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
