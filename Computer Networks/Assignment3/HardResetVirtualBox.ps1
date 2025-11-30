be9ae1a4825ee28e20532eac165bd390a1688ae4
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-11-30 15:49:19 -0500

    Create HardResetVirtualBox.ps1

---
+++ Computer Networks/Assignment3/HardResetVirtualBox.ps1
@@ -0,0 +1,15 @@
+﻿taskkill /F /IM VirtualBox.exe
+taskkill /F /IM VBoxSVC.exe
+taskkill /F /IM VBoxHeadless.exe
+taskkill /F /IM VBoxNetAdp.exe
+taskkill /F /IM VBoxNetDhcp.exe
+
+
+net stop vboxdrv
+net stop vboxnetadp
+net stop vboxnetdhcp
+
+
+net start vboxdrv
+net start vboxnetadp
+net start vboxnetdhcp
